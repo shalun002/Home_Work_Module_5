1c2e76812cd38b7d032742b4f198982482314c0a
shalun002 <32149071+shalun002@users.noreply.github.com>
2018-05-02 14:22:06 +0600

    Add project files.

---
+++ Module_5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Url.Module;
+
+///1.	Перехватить исключение запроса к несуществующему веб ресурсу и вывести сообщение о том, что произошла ошибка. Программа должна завершиться без ошибок.
+///
+
+namespace Module_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnterUrl url = new EnterUrl();
+
+            url.SetUrl();
+        }       
+    }
+}
---
+++ Url/Module/EnterUrl.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Url.Module
+{
+    public class EnterUrl
+    {
+        public void SetUrl()
+        {
+            try
+            {
+                string url = Console.ReadLine();
+                string temp = Get(url);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Page not found " + ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        public static string Get(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
